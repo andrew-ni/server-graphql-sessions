e19315b8b5c4dbe7c071a3d5c943b0da8a69fd53
Andrew Ni <niandrewdev@gmail.com>
2022-02-06 03:52:40 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Basic server with authentication
+
+Components
+- apollo-server-express
+- typescript
+- typeorm
+- express-session
+- bcryptjs
+
+1. set up database settings in `ormconfig.json`
+2. yarn
+3. yarn start
+4. configure apollo studio
+    - update connection settings to include cookies
+    - add default header `x-forwarded-proto: https`
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import * as express from 'express';
+import * as session from 'express-session';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { resolvers } from './resolvers';
+import { typeDefs } from './typeDefs';
+
+const startServer = async (port: number) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }: any) => ({ req }),
+  });
+
+  await createConnection();
+
+  const app = express();
+  app.set('trust proxy', process.env.NODE_ENV !== 'production');
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: 'asdfasdfasdfasdf',
+      cookie: {
+        sameSite: 'none',
+        secure: true,
+      },
+    })
+  );
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    cors: {
+      credentials: true,
+      origin: 'https://studio.apollographql.com',
+    },
+  });
+
+  app.listen({ port: port }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer(4000);
