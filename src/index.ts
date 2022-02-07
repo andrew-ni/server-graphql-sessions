2973e4b53776ce53940d516adddeadcc5c5a479e
Andrew Ni <niandrewdev@gmail.com>
2022-02-07 00:45:07 -0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-express';
 import * as express from 'express';
 import * as session from 'express-session';
